d80a24385e8bcbce60dd273a893eb10c542ff84a
fatmazaman <fatma302@gmail.com>
2015-04-21 23:47:44 -0400

    Add two number in different way

--- Add_two_num.py
+++ Add_two_num.py
@@ -1,9 +1,18 @@
+#Add two number without using '+' operator
 def Add_number(x,y):
 	if(x>y):
 		return x*2 - (x-y)
 	else:
 	    return y*2 - (y-x)
 
-print Add_number(4,366)
+#Add two number without using any arithmetic operators
+def Add(x,y):
+    while (b):
+        carry = x & y
+        x = x ^ y
+        y = carry << 1
+    return x 
+
+
 
 
