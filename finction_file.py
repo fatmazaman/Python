88f5925114cf08e7778dc9d1d1517a36eef9af30
fatmazaman <fatma302@gmail.com>
2015-10-04 20:09:54 -0400

    Function with file

--- finction_file.py
+++ finction_file.py
@@ -8,24 +8,23 @@ def print_all(f):
 
 # rewind the content of file
 def rewind(f):
-	print f.seek(0) 
+	print f.seek(0)
 
 #print line by line
 def print_a_line(line_count, f):
 	print line_count, f.readline()
 
-current_file = open(input_file) # open the file
+current_file = open(input_file)
 
 print "First Let's print the whole file:\n"
 print_all(current_file)
 
 
-print "Now let's rewind, kind of like a tape."
-rewind(current_file)
-
 print "Let's prine three lines:"
 
 current_line = 1
 print_a_line (current_line, current_file)
-print_a_line (current_line+1, current_file)
+current_line += 1
+print_a_line (current_line, current_file)
+current_line += 1
 print_a_line (current_line+2, current_file)
